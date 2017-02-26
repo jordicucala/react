7cefe18fa1367c1ddec43dae791d84af7dff473c
Jordi Cucala <jordicucala@hotmail.com>
2017-02-26 23:57:24 +0100

    passing data into our components

--- contact_app/src/App.js
+++ contact_app/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import ContactsList from './ContactsList';
 
+let contacts = {
+  name: 'Scott',
+  phone: '555 555 555'
+}
+
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Contacts Lists</h1>
-        <ContactsList />
+        <ContactsList contacts={contacts}/>
       </div>
     );
   }
