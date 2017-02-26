44014845d7e9d4e190e581e9c985b20882127c34
Jordi Cucala <jordicucala@hotmail.com>
2017-02-27 00:34:26 +0100

    Search

--- contact_app/src/ContactsList.js
+++ contact_app/src/ContactsList.js
@@ -2,13 +2,33 @@ import React from 'react';
 import Contact from './Contact';
 
 class ContactsList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    };
+  }
+
+  updateSearch(event) {
+    console.log(event.target.value);
+    this.setState({search: event.target.value.substr(0,10)});
+  }
+
   render() {
+    let filteredContacts = this.props.contacts.filter(
+      (contact) => {
+        return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+      }
+    );
     return (
+        <div>
         <ul>
-          {this.props.contacts.map((contact) => {
+          {filteredContacts.map((contact) => {
             return <Contact contact={contact} key={contact.id}/>
           })}
         </ul>
+        <input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)}/>
+        </div>
     )
   }
 }
