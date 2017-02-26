0a0a91bdd1db9944effcc14fc9f8ae79689ac9ec
Jordi Cucala <jordicucala@hotmail.com>
2017-02-26 14:08:43 +0100

    added propTypes

--- react_tutorial/src/index.js
+++ react_tutorial/src/index.js
@@ -5,6 +5,15 @@ import './index.css';
 
 
 var HelloWorld = React.createClass({
+
+	propTypes: {
+		name: React.PropTypes.string
+	},
+	getDefaultProps: function () {
+			return {
+				name: 'Jordi'
+			}
+	},
 	render: function(){
 		return (
 			<h1>Hello World {this.props.name}</h1>
@@ -15,6 +24,6 @@ var HelloWorld = React.createClass({
 
 
 ReactDOM.render(
-  <HelloWorld name='Jordi' />,
+  <HelloWorld name={false} />,
   document.getElementById('root')
 );
