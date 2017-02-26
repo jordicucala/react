29690f394b7aa6b510b9aca2ede74424ba0fb998
Jordi Cucala <jordicucala@hotmail.com>
2017-02-26 23:48:51 +0100

    created component and importing and exporting it

--- contact_app/src/App.js
+++ contact_app/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ContactsList from './ContactsList';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Contacts Lists</h1>
+        <ContactsList />
       </div>
     );
   }
